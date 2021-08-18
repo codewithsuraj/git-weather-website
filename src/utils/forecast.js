24a1d1af4c8f3c82c5a8916a8f52294b443c34c2
Suraj Mahendrakar <mail2surajmahendrakar.com>
2021-08-18 16:33:46 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+     const url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&units=metric&appid=0083fb1f70897058bbe0e8e8c8625bcc'
+     
+     request({ url, json: true}, (error, {body}) => {
+          if(error) {
+               callback('Unable to connect to weather service!', undefined)
+          } else if (body.message) {
+               callback('Unable to find location!', undefined)
+          } else{
+               callback(undefined, body.weather[0].description + ', It is currently ' + body.main.temp + ' degress out. It feels like ' + body.main.feels_like +' degress out.');
+          }
+     });
+}
+
+module.exports = forecast
